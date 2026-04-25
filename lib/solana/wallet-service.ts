6d95b534e6f3f1e5b4a5f86a0a3263a795afe0d4
Hkaar <HkaarDev@gmail.com>
2026-04-25 14:03:22 +0800

    feat: added wallet service to generate user wallets

---
+++ lib/solana/wallet-service.ts
@@ -0,0 +1,29 @@
+import { Keypair } from "@solana/web3.js";
+import bs58 from "bs58";
+import { encrypt, decrypt } from "@/lib/solana/crypto-service";
+
+export interface WalletData {
+  address: string;
+  encryptedPrivateKey: string;
+}
+
+/**
+ * Generate a brand-new Solana wallet.
+ * Returns public address + AES-256-GCM encrypted private key.
+ * Store both fields in MongoDB as-is.
+ */
+export function generateWallet(): WalletData {
+  const keypair = Keypair.generate();
+  return {
+    address: keypair.publicKey.toBase58(),
+    encryptedPrivateKey: encrypt(bs58.encode(keypair.secretKey)),
+  };
+}
+
+/**
+ * Reconstruct Keypair from encrypted private key (as stored in DB).
+ */
+export function keypairFromEncrypted(encryptedPrivateKey: string): Keypair {
+  const privateKeyBase58 = decrypt(encryptedPrivateKey);
+  return Keypair.fromSecretKey(bs58.decode(privateKeyBase58));
+}
